5cde26e3b5dc1767c50a2d255fc94659b616ed37
drgauvin <drgauvin@mgmcare.org>
2017-07-14 13:29:03 -0400

    sdf

--- public/bundle.js
+++ public/bundle.js
@@ -19180,6 +19180,10 @@ var _contact = __webpack_require__(485);
 
 var _contact2 = _interopRequireDefault(_contact);
 
+var _register = __webpack_require__(486);
+
+var _register2 = _interopRequireDefault(_register);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _reactDom.render)(_react2.default.createElement(
@@ -19190,7 +19194,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     { path: '/', component: _client2.default },
     _react2.default.createElement(_reactRouter.IndexRoute, { component: _home2.default }),
     _react2.default.createElement(_reactRouter.Route, { path: '/about', component: _about2.default }),
-    _react2.default.createElement(_reactRouter.Route, { path: '/contact', component: _contact2.default })
+    _react2.default.createElement(_reactRouter.Route, { path: '/contact', component: _contact2.default }),
+    _react2.default.createElement(_reactRouter.Route, { path: '/register', component: _register2.default })
   )
 ), document.getElementById('wali'));
 
@@ -44613,6 +44618,60 @@ var Contact = function (_Component) {
 }(_react.Component);
 
 exports.default = Contact;
+
+/***/ }),
+/* 486 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Register = function (_Component) {
+  _inherits(Register, _Component);
+
+  function Register() {
+    _classCallCheck(this, Register);
+
+    return _possibleConstructorReturn(this, (Register.__proto__ || Object.getPrototypeOf(Register)).apply(this, arguments));
+  }
+
+  _createClass(Register, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: 'page-header' },
+        _react2.default.createElement(
+          'h1',
+          null,
+          ' Register '
+        )
+      );
+    }
+  }]);
+
+  return Register;
+}(_react.Component);
+
+exports.default = Register;
 
 /***/ })
 /******/ ]);
